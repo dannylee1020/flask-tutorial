32eccea76b8fc85a8d11bcc467194303e050ba09
dannylee1020 <dannylee1020@gmail.com>
2020-09-22 15:02:19 -0700

    templates

--- microblog/app/routes.py
+++ microblog/app/routes.py
@@ -1,6 +1,21 @@
+from flask import render_template
 from app import app
+
 
 @app.route('/')
 @app.route('/index')
 def index():
-	return 'Hello World!'
+	user = {'username':'Daniel'}
+	posts = [
+		{
+			'author':{'username':'Marius'},
+			'body': 'Awesome weather in LA!'
+		},
+		{
+			'author':{'username':'Olivia'},
+			'body':'Tenet is such a good movie!'
+
+		}
+	]
+	return render_template('index.html', title = 'Home', user=user, posts=posts)
+
