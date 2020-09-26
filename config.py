0fee6b9f5c36ab52cf4238813558df3e34085ebb
dannylee1020 <dannylee1020@gmail.com>
2020-09-26 16:18:13 -0700

    new configs

--- config.py
+++ config.py
@@ -7,5 +7,12 @@ class Config(object):
 	'sqlite:///' + os.path.join(basedir,'app.db')
 	SQLALCHEMY_TRACK_MODIFICATIONS = False
 
+	MAIL_SERVER = os.environ.get('MAIL_SERVER')
+	MAIL_PORT = os.environ.get(('MAIL_PORT') or 25)
+	MAIL_USER_TLS = os.environ.get('MAIL_USER_TLS') is not None
+	MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
+	MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
+	ADMINS = ['test@example.com']
+
 
 	
