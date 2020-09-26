391bc80b789255665ebaf1d19a18822c027c0c0c
dannylee1020 <dannylee1020@gmail.com>
2020-09-25 17:12:29 -0700

    db migration scripts

---
+++ app/migrations/versions/fd4c340b9d72_.py
@@ -0,0 +1,24 @@
+"""empty message
+
+Revision ID: fd4c340b9d72
+Revises: 1f0060277e4a
+Create Date: 2020-09-25 16:12:01.519439
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'fd4c340b9d72'
+down_revision = '1f0060277e4a'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
